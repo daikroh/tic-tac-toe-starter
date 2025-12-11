3d783967eb16a5ddc106be863e9e104e07839d8f
David Roh <97131738+daikroh@users.noreply.github.com>
2025-12-11 08:53:20 -0800

    adding errors

--- ttt.js
+++ ttt.js
@@ -1,7 +1,7 @@
 function main() {
     console.log(`Lets play tic tac toe!`);
-   let board = init_board();
-   print_board(board)
+    let board = init_board();
+    print_board(board);
 }
 
 function init_board() {
@@ -22,13 +22,26 @@ function print_board(board) {
         for(let j = 0; j < 3; j++) {
             process.stdout.write(board[i][j]);
             if (j !== 2) {
-                process.stdout.write(' | ')
+                process.stdout.write(' | ');
             } else {
-                process.stdout.write('\n')
+                process.stdout.write('\n');
             }
         } if (i !== 2) {
-            console.log('--+---+--')
+            console.log('--+---+--');
         }
     }
+}
+
+function input_error(input) {
+    if (input < 0 || input > 2) {
+        console.log("Please put a proper value!");
+        return false;
+    } return true;
+}
+function board_error(x, y, board) {
+    if (board[x][y] !== '_') {
+        console.log('Please select an empty space!');
+        return false;
+    } return true;
 }
 main()
