0c7ec72abc0f55ed2b9389f8d0bc86b6670a2e81
David Roh <97131738+daikroh@users.noreply.github.com>
2025-12-10 22:53:52 -0800

    finished init_board and print_board functions

--- ttt.js
+++ ttt.js
@@ -1,10 +1,34 @@
 function main() {
-    console.log(`Lets play tic tac toe!`)
-    console.log(`   |   |   
------------
-   |   |   
------------
-   |   |`)
+    console.log(`Lets play tic tac toe!`);
+   let board = init_board();
+   print_board(board)
 }
 
+function init_board() {
+    let board = [
+        ['_', '_', '_'],
+        ['_', '_', '_'],
+        ['_', '_', '_'],
+    ];
+    return board;
+}
+
+/**
+ * 
+ * @param {number[][]} board - 2D Array
+ */
+function print_board(board) {
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            process.stdout.write(board[i][j]);
+            if (j !== 2) {
+                process.stdout.write(' | ')
+            } else {
+                process.stdout.write('\n')
+            }
+        } if (i !== 2) {
+            console.log('--+---+--')
+        }
+    }
+}
 main()
